596030b6a832d914b967d3b6e346e16b8a575bdd
alexou <alexandrevalin2@gmail.com>
2022-10-30 22:14:53 -0400

    asd

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose')
 const articleRouter = require('./routes/articles')
 const app = express()
 
-mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology: true
-})
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology: true})
 
 
 app.set('view engine', 'ejs')
