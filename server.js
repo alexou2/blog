ea7d4afb6a69d6387b5eaf1a46eae530566d618f
_alexou_ <92472779+alexouuuuuuuuu@users.noreply.github.com>
2022-11-02 13:49:27 -0400

    functions added

--- server.js
+++ server.js
@@ -1,27 +1,21 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
 const app = express()
 
-mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology: true})
-
+mongoose.connect('mongodb://localhost/test', { 
+    useNewUrlParser: true, useUnifiedTopology: true
+})
 
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
 
-
-app.use(express.urlencoded({extended: false}))
-
-app.get('/', (req, res) => {
-    const articles = [{
-        title: 'test',
-        createdAt: new Date(),
-        desctiption: 'test description'
-    },
-    {
-        title: 'test 1',
-        createdAt: new Date(),
-        desctiption: 'test description 1'
-    }]
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({
+    createdAt: 'desc' })
     res.render('articles/index', { articles: articles })
 })
 
